3e4ffd2e391a16253a1957ed87f1b577f7207f11
Travis Keep <keep94@gmail.com>
2020-09-04 14:50:24 -0700

    gofmt everything.

--- common.go
+++ common.go
@@ -8,14 +8,14 @@
 package common
 
 import (
-  "reflect"
+	"reflect"
 )
 
 // Join implements the composite patten.
 // Let T be an arbitrary interface.
 // aSlice is a []T consisting of the T values to be joined.
 // aggregate is an arbitrary value of type U that implements T and has an
-// underlying type []T. 
+// underlying type []T.
 // Caller usually passes nil for aggregate as aggregate is only used for
 // reflection.
 // none is of some arbitrary type V that implements T and represents no T
@@ -27,53 +27,53 @@ import (
 // in aSlice flattened out. That is, the returned U value will not contain
 // any additional U values or V values.
 func Join(
-    aSlice interface{},
-    aggregate interface{},
-    none interface{}) interface{} {
-  // -1 = no not none index; -2 = more than one not none index
-  notNoneIdx := -1
-  computedLength := 0
-  sliceVal := reflect.ValueOf(aSlice)
-  noneType := reflect.TypeOf(none)
-  aggregateType := reflect.TypeOf(aggregate)
-  sliceLength := sliceVal.Len()
-  for i := 0; i < sliceLength; i++ {
-    aVal := sliceVal.Index(i).Elem()
-    aType := aVal.Type()
-    if aType != noneType {
-      if notNoneIdx == -1 {
-        notNoneIdx = i
-      } else {
-        notNoneIdx = -2
-      }
-      if aType == aggregateType {
-        computedLength += aVal.Len()
-      } else {
-        computedLength++
-      }
-    } 
-  }
-  if notNoneIdx == -1 {
-    return none
-  }
-  if notNoneIdx >= 0 {
-    return sliceVal.Index(notNoneIdx).Interface()
-  }
-  result := reflect.MakeSlice(
-      aggregateType, computedLength, computedLength)
-  idx := 0
-  for i := 0; i < sliceLength; i++ {
-    aiVal := sliceVal.Index(i)
-    aVal := aiVal.Elem()
-    aType := aVal.Type()
-    if aType != noneType {
-      if aType == aggregateType {
-        idx += reflect.Copy(result.Slice(idx, computedLength), aVal)
-      } else {
-        result.Index(idx).Set(aiVal)
-        idx++
-      }
-    }
-  }
-  return result.Interface()
+	aSlice interface{},
+	aggregate interface{},
+	none interface{}) interface{} {
+	// -1 = no not none index; -2 = more than one not none index
+	notNoneIdx := -1
+	computedLength := 0
+	sliceVal := reflect.ValueOf(aSlice)
+	noneType := reflect.TypeOf(none)
+	aggregateType := reflect.TypeOf(aggregate)
+	sliceLength := sliceVal.Len()
+	for i := 0; i < sliceLength; i++ {
+		aVal := sliceVal.Index(i).Elem()
+		aType := aVal.Type()
+		if aType != noneType {
+			if notNoneIdx == -1 {
+				notNoneIdx = i
+			} else {
+				notNoneIdx = -2
+			}
+			if aType == aggregateType {
+				computedLength += aVal.Len()
+			} else {
+				computedLength++
+			}
+		}
+	}
+	if notNoneIdx == -1 {
+		return none
+	}
+	if notNoneIdx >= 0 {
+		return sliceVal.Index(notNoneIdx).Interface()
+	}
+	result := reflect.MakeSlice(
+		aggregateType, computedLength, computedLength)
+	idx := 0
+	for i := 0; i < sliceLength; i++ {
+		aiVal := sliceVal.Index(i)
+		aVal := aiVal.Elem()
+		aType := aVal.Type()
+		if aType != noneType {
+			if aType == aggregateType {
+				idx += reflect.Copy(result.Slice(idx, computedLength), aVal)
+			} else {
+				result.Index(idx).Set(aiVal)
+				idx++
+			}
+		}
+	}
+	return result.Interface()
 }
--- common_test.go
+++ common_test.go
@@ -6,114 +6,114 @@
 package common_test
 
 import (
-  "fmt"
-  "strings"
-  "testing"
-  "github.com/keep94/common"
+	"fmt"
+	"github.com/keep94/common"
+	"strings"
+	"testing"
 )
 
 var (
-  kNone1 noneType = 1
-  kNone2 noneType = 2
-  kNone3 noneType = 3
-  kSingle1 singleType = "one"
-  kSingle2 singleType = "two"
-  kSingle3 singleType = "three"
-  kMulti456 = multiType{
-      singleType("four"),
-      singleType("five"),
-      singleType("six"),
-  }
-  kMulti78 = multiType{
-      singleType("seven"),
-      singleType("eight"),
-  }
+	kNone1    noneType   = 1
+	kNone2    noneType   = 2
+	kNone3    noneType   = 3
+	kSingle1  singleType = "one"
+	kSingle2  singleType = "two"
+	kSingle3  singleType = "three"
+	kMulti456            = multiType{
+		singleType("four"),
+		singleType("five"),
+		singleType("six"),
+	}
+	kMulti78 = multiType{
+		singleType("seven"),
+		singleType("eight"),
+	}
 )
 
 func TestControl(t *testing.T) {
-  arr := multiType{
-      kNone2,
-      kMulti78,
-      kSingle1,
-  }
-  expected := "[NONE [seven eight] one]"
-  assertStrEqual(t, expected, arr.ToString())
+	arr := multiType{
+		kNone2,
+		kMulti78,
+		kSingle1,
+	}
+	expected := "[NONE [seven eight] one]"
+	assertStrEqual(t, expected, arr.ToString())
 }
 
 func TestNone(t *testing.T) {
-  assertStrEqual(t, "NONE", join(nil))
-  assertStrEqual(t, "NONE", join([]fooType{kNone1, kNone2, kNone3}))
+	assertStrEqual(t, "NONE", join(nil))
+	assertStrEqual(t, "NONE", join([]fooType{kNone1, kNone2, kNone3}))
 }
 
 func TestSingle(t *testing.T) {
-  assertStrEqual(t, "one", join([]fooType{kSingle1}))
-  assertStrEqual(t, "one", join([]fooType{kNone1, kSingle1, kNone2}))
+	assertStrEqual(t, "one", join([]fooType{kSingle1}))
+	assertStrEqual(t, "one", join([]fooType{kNone1, kSingle1, kNone2}))
 }
 
 func TestMulti(t *testing.T) {
-  assertStrEqual(
-      t,
-      "[one three four five six two seven eight]",
-      join([]fooType{
-          kNone1,
-          kSingle1,
-          kSingle3,
-          kNone2,
-          kMulti456,
-          kSingle2,
-          kMulti78,
-          kNone3,
-      }))
-  assertStrEqual(
-      t,
-      "[four five six]",
-      join([]fooType{
-          kNone1,
-          kMulti456,
-          kNone3,
-      }))
-  assertStrEqual(
-      t,
-      "[four five six]",
-      join([]fooType{
-          kMulti456,
-      }))
+	assertStrEqual(
+		t,
+		"[one three four five six two seven eight]",
+		join([]fooType{
+			kNone1,
+			kSingle1,
+			kSingle3,
+			kNone2,
+			kMulti456,
+			kSingle2,
+			kMulti78,
+			kNone3,
+		}))
+	assertStrEqual(
+		t,
+		"[four five six]",
+		join([]fooType{
+			kNone1,
+			kMulti456,
+			kNone3,
+		}))
+	assertStrEqual(
+		t,
+		"[four five six]",
+		join([]fooType{
+			kMulti456,
+		}))
 }
 
 func join(arr []fooType) string {
-  var agg multiType
-  var none noneType
-  return common.Join(arr, agg, none).(fooType).ToString()
+	var agg multiType
+	var none noneType
+	return common.Join(arr, agg, none).(fooType).ToString()
 }
 
 func assertStrEqual(t *testing.T, expected, actual string) {
-  if expected != actual {
-    t.Errorf("Expected %s, got %s", expected, actual)
-  }
+	if expected != actual {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
 }
 
 type fooType interface {
-  ToString() string
+	ToString() string
 }
 
 type noneType int
 
 func (n noneType) ToString() string {
-  return "NONE"
+	return "NONE"
 }
 
 type singleType string
 
 func (s singleType) ToString() string {
-  return string(s)
+	return string(s)
 }
 
 type multiType []fooType
 
 func (m multiType) ToString() string {
-  strs := make([]string, len(m))
-  for i := range m {
-    strs[i] = m[i].ToString()
-  }
-  return fmt.Sprintf("[%s]", strings.Join(strs, " "))
+	strs := make([]string, len(m))
+	for i := range m {
+		strs[i] = m[i].ToString()
+	}
+	return fmt.Sprintf("[%s]", strings.Join(strs, " "))
 }
